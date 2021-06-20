797a41cb107ef783bbdd71da7839c13da92ebe10
Stache <hugo.hueber@epfl.ch>
2021-06-20 19:13:25 +0200

    vroum #2 #3

--- main.py
+++ main.py
@@ -53,25 +53,35 @@ def start(update: Update, context: CallbackContext) -> None:
         reply_markup=ForceReply(selective=True),
     )
 
+    logger.info("%s says hi!", user.first_name)
+
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
+
+    logger.info("%s needs help!", update.effective_user.first_name)
 
 
 def vroum(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Vroum!')
 
+    logger.info("%s gets a Vroum!", update.effective_user.first_name)
+
 
 def vroom(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('😠')
 
+    logger.info("%s gets a 😠!", update.effective_user.first_name)
+
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     update.message.reply_text(update.message.text)
+
+    logger.info("%s want an echo!", update.effective_user.first_name)
 
 
 def plus(update: Update, context: CallbackContext) -> None:
@@ -81,6 +91,8 @@ def plus(update: Update, context: CallbackContext) -> None:
 
         if user == update.effective_user:
             update.message.reply_text("Humble bragging, amarite?")
+
+            logger.info("%s wants to pos themselves!", user.first_name)
             return
 
         dbuser = get_user(user.id, update.message.chat.id)
@@ -88,6 +100,8 @@ def plus(update: Update, context: CallbackContext) -> None:
         dbuser.save()
 
         update.message.reply_to_message.reply_text("+1 for {} ({} points).".format(user.first_name, dbuser.karma))
+
+        logger.info("%s gets a +1!", user.first_name)
 
 
 def moins(update: Update, context: CallbackContext) -> None:
@@ -97,6 +111,8 @@ def moins(update: Update, context: CallbackContext) -> None:
 
         if user == update.effective_user:
             update.message.reply_text("Don't be so harsh on yourself.")
+
+            logger.info("%s wants to neg themselves!", user.first_name)
             return
 
         dbuser = get_user(user.id, update.message.chat.id)
@@ -104,6 +120,8 @@ def moins(update: Update, context: CallbackContext) -> None:
         dbuser.save()
 
         update.message.reply_to_message.reply_text("-1 for {} ({} points).".format(user.first_name, dbuser.karma))
+
+        logger.info("%s gets a -1!", user.first_name)
 
 
 def getkarma(update: Update, context: CallbackContext) -> None:
@@ -116,6 +134,8 @@ def getkarma(update: Update, context: CallbackContext) -> None:
     karma = get_user(user.id, update.message.chat.id).karma
     update.message.reply_text("{} has {} points.".format(user.first_name, karma))
 
+    logger.info("%s has %d karma!", user.first_name, karma)
+
 
 def userid(update: Update, context: CallbackContext) -> None:
     if update.message.reply_to_message:
@@ -125,6 +145,8 @@ def userid(update: Update, context: CallbackContext) -> None:
         user = update.effective_user
         update.message.reply_text(user.id)
 
+    logger.info("%s wants their ID! It is .", user.first_name, user.id)
+
 
 def random_cat(update: Update, context: CallbackContext) -> None:
     folder = "./cats"
@@ -132,12 +154,37 @@ def random_cat(update: Update, context: CallbackContext) -> None:
     meow = random.choice(["meo", "meong", "meow", "mèu", "miaau", "miaou", "miau", "miauw", "喵", "喵", "喵", "miao", "miaow", "miyav", "miav", "mjau", "միյաւ", "ম্যাঁও", "meogre", "miaŭ", "ngiyaw", "میاؤں", "mjá", "mňau", "ngeung", "კნავილი", "njäu", "ņau", "nyav", "mi'au", "νιάου", "にゃー", "야옹", "냥", "מיאו", "מיאַו", "мјау", "miáú", "miau", "nyaú", "мяу", "ngiau", "mijav", "mia'wj", "میو", "مُواء"])
     update.message.reply_photo(photo=open(filename, 'rb'), caption=meow)
 
+    logger.info("%s wants a cat pic!", update.effective_user.first_name)
+
 
 def brrou(update: Update, context: CallbackContext) -> None:
     folder = "./brrou"
     filename = os.path.join(folder, random.choice(os.listdir(folder)))
     meow = random.choice(["brrou", "Brrou", "b r r o u", "BROU", "B R R O U", "tut"])
     update.message.reply_photo(photo=open(filename, 'rb'), caption=meow)
+
+    logger.info("%s wants a Brrou pic!", update.effective_user.first_name)
+
+
+def boop(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    if "/beep" in update.message.text:
+        text = "boop"
+    elif "/boop" in update.message.text:
+        text = "beep"
+    else:
+        text = "..."
+
+    update.message.reply_text(text)
+
+    logger.info("%s gets a %s!", update.effective_user.first_name, text)
+
+
+def tut(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text("tut")
+
+    logger.info("%s gets a tut!", update.effective_user.first_name)
 
 
 def get_user(userid, chatid):
@@ -184,6 +231,11 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("kot", random_cat))
 
     dispatcher.add_handler(CommandHandler("brrou", brrou))
+
+    dispatcher.add_handler(CommandHandler("tut", tut))
+
+    dispatcher.add_handler(CommandHandler("boop", boop))
+    dispatcher.add_handler(CommandHandler("beep", boop))
     # dispatcher.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
