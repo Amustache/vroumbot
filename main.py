63113ea25697b18394a1ba332fc6ce3736121490
Stache <hugo.hueber@epfl.ch>
2021-10-25 18:20:31 +0200

    vroum... vroum...
    Add sciper functionnalities

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import os
 import random
 
 
+from bs4 import BeautifulSoup
 from telegram import ForceReply, Update
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
+import requests
 
 
 from helpers import DB, get_user, User
@@ -379,9 +381,61 @@ def feedback(update: Update, context: CallbackContext) -> None:
 
 
 def carpe(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
     if update.effective_user.username == "ReallyCrazyMan" and random.randint(1, 6) == 6:
         update.message.reply_photo(photo=open("./media/opinion.jpg", "rb"))
+
+
+def whoissciper(update: Update, context: CallbackContext) -> None:
+    _, sciper = update.message.text.split(" ", 1)
+    try:
+        sciper = int(sciper)
+        if sciper < 100000 or sciper > 999999:
+            raise Exception  # epic coding
+    except:
+        update.message.reply_text("Not a valid SCIPER.")
+        return
+
+    URL = "https://people.epfl.ch/{}".format(sciper)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    try:
+        name = soup.find_all("h1", class_="mr-3")[0].text
+    except:
+        update.message.reply_text("SCIPER not found or not public.")
+        return
+
+    update.message.reply_text("{} is {}.".format(sciper, name))
+
+
+def whoisscipernsfw(update: Update, context: CallbackContext) -> None:
+    _, sciper = update.message.text.split(" ", 1)
+    try:
+        sciper = int(sciper)
+        if sciper < 100000 or sciper > 999999:
+            raise Exception  # epic coding
+    except:
+        update.message.reply_text("Not a valid SCIPER b-baka.")
+        return
+
+    URL = "https://nhentai.to/g/{}".format(sciper)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    if soup.title.text == "Not Found":
+        update.message.reply_text("{} seems to be pure and innocent...".format(sciper))
+        return
+
+    tags = [thing for thing in soup.findAll("div", {"class": "tag-container"}) if "Tags" in thing.text]
+    if tags:
+        tags_text = []
+        for tag in [tag.text.strip() for tag in tags[0].findAll("a", {"class": "tag"})]:
+            tags_text.append(tag)
+        if tags_text:
+            update.message.reply_text("{} is linked to {}.".format(sciper, ", ".join(tags_text)))
+            return
+
+    update.message.reply_text("{} is a secretive pervert.".format(sciper))
 
 
 def main() -> None:
@@ -440,6 +494,9 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler(["genre", "gender", "sexe", "sex", "sexx", "genr"], gender))
 
     dispatcher.add_handler(MessageHandler(~Filters.command, carpe))
+
+    dispatcher.add_handler(CommandHandler(["whois", "whoissciper", "sciper"], whoissciper))
+    dispatcher.add_handler(CommandHandler(["whoisnsfw", "whoisscipernsfw", "scipernsfw"], whoisscipernsfw))
 
     dispatcher.add_handler(CommandHandler("all_commands", help_command))
 
