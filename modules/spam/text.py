38450aad0a46a855976bd37b702c0b135aa856d1
Stache <hugo.hueber@epfl.ch>
2022-01-05 14:38:53 +0100

    vroum vroum vroum
    Add peptalk

--- modules/spam/text.py
+++ modules/spam/text.py
@@ -27,6 +27,7 @@ class Text(Base):
             CommandHandler(["keysmash", "bottom", "helo"], self.keysmash),
             CommandHandler(["oh", "ooh", "oooh"], self.oh),
             CommandHandler(["ay", "ayy", "ayyy", "xd", "xdd", "xddd"], self.xd),
+            CommandHandler(["pep", "peptalk", "motivation", "motivational"], self.peptalk),
         ]
         super().__init__(logger, commandhandlers)
 
@@ -145,3 +146,101 @@ class Text(Base):
         update.message.reply_text(result)
 
         self.logger.info("{} is in XDing real hard!".format(update.effective_user.first_name))
+
+    def peptalk(self, update: Update, context: CallbackContext) -> None:
+        """
+        When you need a bit of motivation!
+        """
+        first = [
+            "Champ,",
+            "Fact:",
+            "Everybody says",
+            "Dang...",
+            "Check it:",
+            "Just saying...",
+            "Superstar,",
+            "Tiger,",
+            "Self,",
+            "Know this:",
+            "News alert:",
+            "Girl,",
+            "Ace,",
+            "Excuse me but",
+            "Experts agree",
+            "In my opinion,",
+            "Hear ye, hear ye:",
+            "Okay, listen up:",
+        ]
+
+        second = [
+            "the mere idea of you",
+            "your soul",
+            "your hair today",
+            "everything you do",
+            "your personal style",
+            "every thought you have",
+            "that sparkle in your eye",
+            "your presence here",
+            "what you got going on",
+            "the essential you",
+            "your life's journey",
+            "that saucy personlity",
+            "your DNA",
+            "that brain of yours",
+            "your choice of attire",
+            "the way you roll",
+            "whatever your secret is",
+            "all of y'all",
+        ]
+
+        third = [
+            "has serious game,",
+            "rains magic,",
+            "deserves the Nobel Prize,",
+            "raises the roof,",
+            "breeds miracles,",
+            "is paying off big time,",
+            "shows mad skills,",
+            "just shimmers,",
+            "is a national treasure,",
+            "gets the party hopping,",
+            "is the next big thing,",
+            "roars like a lion,",
+            "is a rainbow factory,",
+            "is made of diamonds,",
+            "makes birds sing,",
+            "should be taught in school,",
+            "makes my world go 'round,",
+            "is 100% legit,",
+        ]
+
+        fourth = [
+            "24/7.",
+            "can I get an amen?",
+            "and that's a fact.",
+            "so treat yourself.",
+            "you feel me?",
+            "that's just science.",
+            "would I lie?",
+            "for reals.",
+            "mic drop.",
+            "you hidden gem.",
+            "snuggle bear.",
+            "period.",
+            "now let's dance.",
+            "high five.",
+            "say it again!",
+            "according to CNN.",
+            "so get used to it.",
+        ]
+
+        update.message.reply_text(
+            "{} {} {} {}".format(
+                random.choice(first),
+                random.choice(second),
+                random.choice(third),
+                random.choice(fourth),
+            )
+        )
+
+        self.logger.info("{} gets a little motivation!".format(update.effective_user.first_name))
