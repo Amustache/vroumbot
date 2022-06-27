204949a459337a14f9e2786f3425342d0f9ec90e
Stache <stache@stache.cat>
2022-06-27 10:20:16 +0200

    wip

--- databases.py
+++ databases.py
@@ -46,6 +46,35 @@ class ChatCommand(Model):
         database = main_db
 
 
+class ChatJob(Model):
+    """
+    Chat command model to access the database.
+    """
+
+    chatid = BigIntegerField()
+    messageid = BigIntegerField()
+    deadline = DateTimeField()
+
+    class Meta:
+        """
+        Basically which database.
+        """
+
+        database = main_db
+
+
+for job in ChatJob.select():
+    delta = (job.deadline - datetime.datetime.now()) + datetime.timedelta(seconds=1)
+    if delta.total_seconds() < 1:
+        job.delete_instance()
+    else:
+        dispatcher.job_queue.run_once(
+            None,
+            delta.total_seconds(),
+            context={"chat_id": job.chatid, "message_id": job.messageid},
+            name="{}_{}".format(job.chatid, job.messageid),
+        )
+
 # class ChatModule(Model):
 #     """
 #     Chat command model to access the database.
